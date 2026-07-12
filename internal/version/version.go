906d5f6e65daf233982808da779338cd465cdb0b
Boii <i@tcp404.com>
2026-07-12 15:38:33 +0800

    build(updater): align release version injection

--- internal/version/version.go
+++ internal/version/version.go
@@ -7,9 +7,3 @@ const (
 	VersionLatestURL = "https://api.github.com/repos/tcp404/OneTiny/releases/latest"
 	VersionByTagURL  = "https://api.github.com/repos/tcp404/OneTiny/releases/tags/"
 )
-
-var ReleaseName = map[string]string{
-	"linux":   "OneTiny",
-	"windows": "OneTiny.exe",
-	"darwin":  "OneTiny_mac",
-}
